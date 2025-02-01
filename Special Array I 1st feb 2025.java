134c8e55008d385a1dfd56328ae5995e14312949
Pratham Kubetkar <kubetkarpratham14@gmail.com>
2025-02-01 09:49:16 +0530

    Create Special Array I 1st feb 2025.java

---
+++ Special Array I 1st feb 2025.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length < 2) return true;
+        for(int i=0;i<nums.length-1;i++){
+            if((nums[i]%2 == nums[i+1]%2) ) return false;
+        }
+        return true;
+    }
+}
